b8ea3c0b4db1f3863032f287df91b2c4f2190d63
Bernardo Loureiro <bernardo.pacheco@gmail.com>
2025-07-23 18:39:33 -0300

    Standardize indigenous territories and protected areas

---
+++ scripts/boundaries/standardize_it_and_pa_areas.py
@@ -0,0 +1,136 @@
+# Standardizes Indigenous Territories and
+# Protected Areas from source shapefiles.
+
+# You can run this script with uv if you prefer,
+# see https://docs.astral.sh/uv/guides/scripts/.
+# To run: `uv run scripts/boundaries/standardize_it_and_pa_areas.py`.
+
+# to list all shapefiles in folder:
+# find ./data/boundaries/protected_areas_and_indigenous_territories -name "*.shp" -type f | sed 's|^\./||' | sort
+
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "chardet",
+#     "geopandas",
+#     "numpy",
+# ]
+# ///
+import chardet
+import geopandas as gpd
+import numpy as np
+import json
+import os
+from pathlib import Path
+
+SOURCE_DATA_FOLDER = (
+    "data/boundaries/protected_areas_and_indigenous_territories/source_data"
+)
+OUTPUT_DATA_FOLDER = "data/boundaries/protected_areas_and_indigenous_territories/out"
+SIMPLIFY_TOLERANCE = 0.001
+
+with open("scripts/boundaries/files_metadata.json") as f:
+    files_metadata = json.load(f)
+
+
+def detect_shapefile_encoding(shapefile_path):
+    # get the .dbf file path
+    dbf_path = shapefile_path.replace(".shp", ".dbf")
+
+    with open(dbf_path, "rb") as f:
+        raw_data = f.read()
+        result = chardet.detect(raw_data)
+        return result["encoding"]
+
+
+def combine_and_save_frames(all_frames, output_folder, filename, simplify):
+    # combine frames
+    combined_gdf = gpd.pd.concat(all_frames, ignore_index=True)
+    output_combined_file = f"{output_folder}/{filename}"
+
+    # ensure output directory exists
+    output_path = Path(output_combined_file)
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+
+    # simplify geometries
+    if simplify:
+        combined_gdf["geometry"] = combined_gdf["geometry"].simplify(
+            tolerance=SIMPLIFY_TOLERANCE, preserve_topology=True
+        )
+    
+    # create an unique ID
+    combined_gdf = combined_gdf.reset_index(drop=False)
+    combined_gdf = combined_gdf.rename(columns={'index': 'id'})
+
+    # save combined file
+    combined_gdf.to_file(output_combined_file, driver="GeoJSON", encoding="utf-8")
+    print(f"Created: {output_combined_file}")
+
+
+def standardize_and_combine_shapefiles(files_metadata):
+    indigenous_territories = []
+    protected_areas = []
+    for file in files_metadata:
+        # a dictionary to rename the columns in the data
+        field_cols_rename_dict = {
+            v: k for k, v in file.items() if k.endswith("_field") and v != ""
+        }
+
+        # # the columns we will select from the dataframe
+        # cols_to_select = [v for k, v in field_cols_rename_dict.items()]
+
+        # read file
+        file_path = f"{SOURCE_DATA_FOLDER}/{file["file"]}"
+        print(f"Reading {file_path}")
+
+        try:
+            # assume first it's in utf-8
+            gdf = gpd.read_file(file_path, encoding="utf-8")
+        except:
+            # if gpd can't read the file with utf-8, try to detect the encoding
+            detected_encoding = detect_shapefile_encoding(file_path)
+            print(f"Detected encoding: {detected_encoding}")
+            gdf = gpd.read_file(file_path, encoding=detected_encoding)
+
+        # rename
+        gdf = gdf.rename(columns=field_cols_rename_dict)
+
+        # all of the required columns
+        cols_to_export = [k for k in file.keys() if k.endswith("_field")]
+        # add missing columns
+        missing_cols = {col: np.nan for col in cols_to_export if col not in gdf.columns}
+        gdf = gdf.assign(**missing_cols)
+
+        # standardize crs
+        gdf = gdf.to_crs("EPSG:4326")
+
+        # include area units and country name from file metadata
+        gdf["area_units"] = file["area_units"]
+        gdf["country"] = file["country"]
+
+        # include other columns
+        cols_to_export = ["country"] + cols_to_export + ["area_units", "geometry"]
+
+        # add to lists
+        if file["type"] == "indigenous-territory":
+            indigenous_territories.append(gdf[cols_to_export])
+        elif file["type"] == "protected-area":
+            protected_areas.append(gdf[cols_to_export])
+        else:
+            print(f"Error, unrecognized data type: {file['type']}")
+            raise ValueError
+
+    # combine frames and save to file
+    combine_and_save_frames(
+        indigenous_territories,
+        OUTPUT_DATA_FOLDER,
+        "indigenous_territories.geojson",
+        simplify=True,
+    )
+    combine_and_save_frames(
+        protected_areas, OUTPUT_DATA_FOLDER, "protected_areas.geojson", simplify=True
+    )
+
+
+if __name__ == "__main__":
+    standardize_and_combine_shapefiles(files_metadata)
