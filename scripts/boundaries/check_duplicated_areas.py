846da34191ac76832e5b2efc0bd347ad9fa3ef13
Bernardo Loureiro <bernardo.pacheco@gmail.com>
2025-08-05 15:42:36 -0300

    Update script to check duplicated areas

--- scripts/boundaries/check_duplicated_areas.py
+++ scripts/boundaries/check_duplicated_areas.py
@@ -1,3 +1,7 @@
+# You can run this script with uv if you prefer,
+# see https://docs.astral.sh/uv/guides/scripts/.
+# To run: `uv run scripts/boundaries/check_duplicated_areas.py`.
+
 # /// script
 # requires-python = ">=3.12"
 # dependencies = [
@@ -10,21 +14,27 @@ import geopandas as gpd
 
 OUTPUT_DATA_FOLDER = "data/boundaries/protected_areas_and_indigenous_territories/out"
 INDIGENOUS_TERRITORIES = f"{OUTPUT_DATA_FOLDER}/indigenous_territories.geojson"
+PROTECTED_AREAS = f"{OUTPUT_DATA_FOLDER}/protected_areas.geojson"
 
 
-def check_duplicated_areas():
-    gdf = gpd.read_file(INDIGENOUS_TERRITORIES)
+def check_duplicated_areas(file_path, output_file_prefix):
+    gdf = gpd.read_file(file_path)
 
     # count by country and name
     gdf.value_counts(["country", "name_field"]).loc[lambda x: x > 1].to_csv(
-        f"{OUTPUT_DATA_FOLDER}/country_name_counts.csv", header=True
+        f"{OUTPUT_DATA_FOLDER}/{output_file_prefix}_country_name_counts.csv",
+        header=True,
     )
 
     # count by country, name and status
     gdf.value_counts(["country", "name_field", "status_field"]).loc[
         lambda x: x > 1
-    ].to_csv(f"{OUTPUT_DATA_FOLDER}/country_name_status_counts.csv", header=True)
+    ].to_csv(
+        f"{OUTPUT_DATA_FOLDER}/{output_file_prefix}_country_name_status_counts.csv",
+        header=True,
+    )
 
 
 if __name__ == "__main__":
-    check_duplicated_areas()
+    check_duplicated_areas(INDIGENOUS_TERRITORIES, "indigenous")
+    check_duplicated_areas(PROTECTED_AREAS, "protected")
