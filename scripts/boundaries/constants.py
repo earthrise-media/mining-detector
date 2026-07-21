c00194da9ef0bd4290ed77e0bab21a809cccce13
Bernardo Loureiro <bernardo.pacheco@gmail.com>
2026-07-21 21:54:38 +0100

    Update source files

--- scripts/boundaries/constants.py
+++ scripts/boundaries/constants.py
@@ -1,26 +1,30 @@
 OUTPUTS_FOLDER = "data/outputs"
 WEBSITE_OUTPUTS_FOLDER = f"{OUTPUTS_FOLDER}/website"
 
-DATA_UPDATED_AT = "20260720"
+DATA_UPDATED_AT = "20260721"
 
 MINING_DIFFERENCES_FILES = {
-    202504: f"{OUTPUTS_FOLDER}/48px_v0.X-SSL4EO-MLPensemble/cumulative/amazon_basin_48px_v0.X-SSL4EO-MLPensemble2025Q4diff-clean-gt11ha.geojson",
-    202503: f"{OUTPUTS_FOLDER}/48px_v0.X-SSL4EO-MLPensemble/cumulative/amazon_basin_48px_v0.X-SSL4EO-MLPensemble2025Q3diff-clean-gt11ha.geojson",
-    202502: f"{OUTPUTS_FOLDER}/48px_v0.X-SSL4EO-MLPensemble/cumulative/amazon_basin_48px_v0.X-SSL4EO-MLPensemble2025Q2diff-clean-gt11ha.geojson",
-    202400: f"{OUTPUTS_FOLDER}/48px_v0.X-SSL4EO-MLPensemble/cumulative/amazon_basin_48px_v0.X-SSL4EO-MLPensemble2024-clean-diff-gt11ha.geojson",
-    202300: f"{OUTPUTS_FOLDER}/48px_v3.2-3.7ensemble/cumulative/amazon_basin_48px_v3.2-3.7ensemble_dissolved-0.6_2023diff.geojson",
-    202200: f"{OUTPUTS_FOLDER}/48px_v3.2-3.7ensemble/cumulative/amazon_basin_48px_v3.2-3.7ensemble_dissolved-0.6_2022diff.geojson",
-    202100: f"{OUTPUTS_FOLDER}/48px_v3.2-3.7ensemble/cumulative/amazon_basin_48px_v3.2-3.7ensemble_dissolved-0.6_2021diff.geojson",
-    202000: f"{OUTPUTS_FOLDER}/48px_v3.2-3.7ensemble/cumulative/amazon_basin_48px_v3.2-3.7ensemble_dissolved-0.6_2020diff.geojson",
-    201900: f"{OUTPUTS_FOLDER}/48px_v3.2-3.7ensemble/cumulative/amazon_basin_48px_v3.2-3.7ensemble_dissolved-0.6_2019diff.geojson",
-    201800: f"{OUTPUTS_FOLDER}/48px_v3.2-3.7ensemble/cumulative/amazon_basin_48px_v3.2-3.7ensemble_dissolved-0.6_2018-2018cumulative.geojson",
+    202602: f"{OUTPUTS_FOLDER}/cumulative_detections/Amazon_ACA_48px_v4.10b-18d-20g-21a-22bc-ensemble_t0.55_d5_3km_t-iso0.8_cumulative2018-Q226-diff.geojson",
+    202601: f"{OUTPUTS_FOLDER}/cumulative_detections/Amazon_ACA_48px_v4.10b-18d-20g-21a-22bc-ensemble_t0.55_d5_3km_t-iso0.8_cumulative2018-Q126-diff.geojson",
+    202504: f"{OUTPUTS_FOLDER}/cumulative_detections/Amazon_ACA_48px_v4.10b-18d-20g-21a-22bc-ensemble_t0.55_d5_3km_t-iso0.8_cumulative2018-Q425-diff.geojson",
+    202503: f"{OUTPUTS_FOLDER}/cumulative_detections/Amazon_ACA_48px_v4.10b-18d-20g-21a-22bc-ensemble_t0.55_d5_3km_t-iso0.8_cumulative2018-Q325-diff.geojson",
+    202502: f"{OUTPUTS_FOLDER}/cumulative_detections/Amazon_ACA_48px_v4.10b-18d-20g-21a-22bc-ensemble_t0.55_d5_3km_t-iso0.8_cumulative2018-Q225-diff.geojson",
+    202501: f"{OUTPUTS_FOLDER}/cumulative_detections/Amazon_ACA_48px_v4.10b-18d-20g-21a-22bc-ensemble_t0.55_d5_3km_t-iso0.8_cumulative2018-Q125-diff.geojson",
+    202400: f"{OUTPUTS_FOLDER}/cumulative_detections/Amazon_ACA_48px_v4.10b-18d-20g-21a-22bc-ensemble_t0.55_d5_3km_t-iso0.8_cumulative2018-2024-diff.geojson",
+    202300: f"{OUTPUTS_FOLDER}/cumulative_detections/Amazon_ACA_48px_v4.10b-18d-20g-21a-22bc-ensemble_t0.55_d5_3km_t-iso0.8_cumulative2018-2023-diff.geojson",
+    202200: f"{OUTPUTS_FOLDER}/cumulative_detections/Amazon_ACA_48px_v4.10b-18d-20g-21a-22bc-ensemble_t0.55_d5_3km_t-iso0.8_cumulative2018-2022-diff.geojson",
+    202100: f"{OUTPUTS_FOLDER}/cumulative_detections/Amazon_ACA_48px_v4.10b-18d-20g-21a-22bc-ensemble_t0.55_d5_3km_t-iso0.8_cumulative2018-2021-diff.geojson",
+    202000: f"{OUTPUTS_FOLDER}/cumulative_detections/Amazon_ACA_48px_v4.10b-18d-20g-21a-22bc-ensemble_t0.55_d5_3km_t-iso0.8_cumulative2018-2020-diff.geojson",
+    201900: f"{OUTPUTS_FOLDER}/cumulative_detections/Amazon_ACA_48px_v4.10b-18d-20g-21a-22bc-ensemble_t0.55_d5_3km_t-iso0.8_cumulative2018-2019-diff.geojson",
+    201800: f"{OUTPUTS_FOLDER}/cumulative_detections/Amazon_ACA_48px_v4.10b-18d-20g-21a-22bc-ensemble_t0.55_d5_3km_t-iso0.8_cumulative2018-2018-diff.geojson",
 }
-
 MINING_YEARS_QUARTERS = sorted(MINING_DIFFERENCES_FILES.keys())
 first_mining_year_quarter, *_, last_mining_year_quarter = MINING_YEARS_QUARTERS
 
-# downloaded from https://source.coop/earthgenome/amazon-mining-watch/current/sam2_mining_scar_masks
+# downloaded from https://source.coop/earthgenome/amazon-mining-watch/mining_scar_masks
 MINING_DIFFERENCES_RASTER_FILES = {
+    202602: "data/outputs/rasters/mining_mask_Q226_full.tif",
+    202601: "data/outputs/rasters/mining_mask_Q126_full.tif",
     202504: "data/outputs/rasters/mining_mask_Q425_full.tif",
     202503: "data/outputs/rasters/mining_mask_Q325_full.tif",
     202502: "data/outputs/rasters/mining_mask_Q225_full.tif",
